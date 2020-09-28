43f4c983e469ff03b3c64f135b077a461101d42e
Tyrel Jackson <tj.dev@Tyrels-MacBook-Pro.local>
2020-09-28 11:17:57 -0600

    Function Const/DS

--- budgety/app.js
+++ budgety/app.js
@@ -1,7 +1,28 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
-   // Some code
+   var Expense = function(id, description, value) {
+       this.id = id;
+       this.description = description;
+       this.value = value;
+   }
+
+   var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 
 })();
 
