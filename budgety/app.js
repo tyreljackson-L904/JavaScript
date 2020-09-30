30e10f935d0c4565a1d316d32ebaef6e9e896813
Tyrel Jackson <tj.dev@Tyrels-MacBook-Pro.local>
2020-09-29 20:51:55 -0600

    Deleting item from Budget Controller

--- budgety/app.js
+++ budgety/app.js
@@ -64,6 +64,20 @@ var budgetController = (function() {
 
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget: function() {
 
             // Calculate total income and expenses
@@ -246,11 +260,13 @@ var controller = (function(budgetCtrl, UICtrl) {
             //inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. Delete the item from the data structure
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
+            //UICtrl.deleteItem(type, id);
 
             // 3. Update and show the new budget
 
