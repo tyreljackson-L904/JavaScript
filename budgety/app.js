75135168aa0ea03a15c7f6199dd150dbea4b5a22
Tyrel Jackson <tj.dev@Tyrels-MacBook-Pro.local>
2020-09-29 21:22:01 -0600

    Percentages

--- budgety/app.js
+++ budgety/app.js
@@ -236,6 +236,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         UICtrl.displayBudget(budget);
     };
 
+    var updatePercentages = function() {
+
+        // 1. Calculate percentages
+
+        // 2. Read from budget controller
+
+        // 3. Update the UI
+
+    };
+
     var ctrlAddItem = function() {
         var input, newItem;
 
@@ -254,6 +264,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
         }
     };
 
@@ -277,6 +290,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 3. Update and show the new budget
             updateBudget();
+
+            // 4. Calculate and update percentages
+            updatePercentages();
 
         }
 
